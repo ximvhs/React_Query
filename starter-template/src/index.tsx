c9b9d475b3a4558e69e16c120009c9515d9f1061
ximvhs <ximvhs26092002@gmail.com>
2024-07-05 10:24:14 +0700

    update

--- starter-template/src/index.tsx
+++ starter-template/src/index.tsx
@@ -4,12 +4,20 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
